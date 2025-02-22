3d8724600745739b5d4b53ce367626323864092a
lemon <l3mon@lemons-MacBook-Air.local>
2025-02-23 00:57:28 +0530

    added proper display messages

--- src/main.cpp
+++ src/main.cpp
@@ -16,29 +16,20 @@ RF24 radio(CE_PIN, CSN_PIN);
 char dataRecived[16];
 bool newData = false;
 
-
-void setup(void) {
-  Serial.begin(9600);
-  delay(1000);
-  Serial.println("SimpleRx Starting");
-  radio.begin();
-  radio.setDataRate(RF24_250KBPS);
-  radio.openReadingPipe(1, thisSlaveAddress);
-  radio.startListening();
+void displayMessage(char message[]){
   u8g2.begin();
   u8g2.clearBuffer();
   u8g2.setFont(u8g2_font_ncenB14_tr);
-  u8g2.drawStr(0, 20, "radio started");
-  delay(1000);
-  sendData();
-}
-
-void loop(){
-  getData();
-  showData();
+  u8g2.drawStr(0, 20, message); 
+  u8g2.sendBuffer();
+  delay(10000);
 }
 
 void sendData(){
+  //screen shits
+  displayMessage("Sending data");
+
+  //sending data
   radio.stopListening();
   char text[] = "Hello Fuckers";
   radio.write(&text, sizeof(text));
@@ -49,20 +40,41 @@ void sendData(){
   radio.startListening();
 }
 
+void setup(void) {
+
+  Serial.begin(9600);
+  Serial.println("SimpleRx Starting");
+
+  displayMessage("Radio initializing..");
+
+  radio.begin();
+  radio.setDataRate(RF24_250KBPS);
+  radio.openReadingPipe(1, thisSlaveAddress);
+  radio.startListening();
+  
+  displayMessage("Radio started");
+
+  sendData();
+}
+
+
 void getData(){
   if(radio.available()){
-    char dataRecived[0] ;
     radio.read(&dataRecived, sizeof(dataRecived));
+    displayMessage(dataRecived);
     newData = true; //for state management
   }
 }
 
 void showData(){
   if(newData == true){
-    u8g2.clearBuffer();
-    u8g2.setFont(u8g2_font_ncenB14_tr);
-    u8g2.drawStr(0, 20, dataRecived);
-    u8g2.sendBuffer();
+    displayMessage(dataRecived);
     newData = false;
   }
 }
+
+void loop(){
+  getData();
+  showData();
+}
+
