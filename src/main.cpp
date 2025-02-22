a8308b6883d5c79343c9e7099e4b8e7ad8a35ec9
lemon <l3mon@lemons-MacBook-Air.local>
2025-02-23 01:33:03 +0530

    radio initliziation debugging

--- src/main.cpp
+++ src/main.cpp
@@ -9,35 +9,36 @@ U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0,  U8X8_PIN_NONE);
 #define CE_PIN 4
 #define CSN_PIN 5
 
-const byte thisSlaveAddress[5] = {'R','x','A','A','A'};
+const byte reciverAddress[5] = {'R','x','A','A','A'};
 
 RF24 radio(CE_PIN, CSN_PIN);
 
 char dataRecived[16];
 bool newData = false;
 
-void displayMessage(char message[]){
+void displayMessage(const char* message){
   u8g2.begin();
   u8g2.clearBuffer();
   u8g2.setFont(u8g2_font_ncenB14_tr);
   u8g2.drawStr(0, 20, message); 
   u8g2.sendBuffer();
-  delay(10000);
+  delay(1000);
 }
 
-void sendData(){
-  //screen shits
-  displayMessage("Sending data");
-
-  //sending data
+void sendData() {
+  displayMessage("Sending...");
+  
   radio.stopListening();
-  char text[] = "Hello Fuckers";
+  char text[] = "Hello World!"; // Keep messages under 16 chars
   radio.write(&text, sizeof(text));
-  u8g2.clearBuffer();
-  u8g2.setFont(u8g2_font_ncenB14_tr);
-  u8g2.drawStr(0, 20, "Data sent");
-  u8g2.sendBuffer();
-  radio.startListening();
+  
+  if(!radio.write(&text, sizeof(text))) {
+    displayMessage("mesage Failed!");
+    
+  } else {
+    displayMessage("Sent OK!");
+  }
+  delay(2000);
 }
 
 void setup(void) {
@@ -47,14 +48,18 @@ void setup(void) {
 
   displayMessage("Radio initializing..");
 
-  radio.begin();
+  bool radioSucess = radio.begin();
+
+  if(!radioSucess){
+    displayMessage("Radio failed to start");
+  }
+  
   radio.setDataRate(RF24_250KBPS);
-  radio.openReadingPipe(1, thisSlaveAddress);
-  radio.startListening();
+  radio.openWritingPipe(reciverAddress); // Receiver's address
+  radio.setRetries(3, 5); // Retry 3x with 5ms delay
   
   displayMessage("Radio started");
 
-  sendData();
 }
 
 
@@ -74,7 +79,8 @@ void showData(){
 }
 
 void loop(){
-  getData();
-  showData();
+  // getData();
+  // showData();
+  sendData(); 
 }
 
